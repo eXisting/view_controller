65f062d224db15da4dceb0320e6ec64d455c76f2
Andrey Popazov <andrey.plagiatix@gmail.com>
2023-06-08 21:48:23 +0300

    Revert send control logic

--- Controller/Assets/Scripts/DTO/ControllerSignal.cs
+++ Controller/Assets/Scripts/DTO/ControllerSignal.cs
@@ -1,10 +1,10 @@
+using System;
 using Enum;
-using Newtonsoft.Json;
 using UnityEngine;
 
 namespace DTO
 {
-  [JsonObject(MemberSerialization.OptOut)]
+  [Serializable]
   public class ControllerSignal
   {
     public ControllerOperation Operation;
